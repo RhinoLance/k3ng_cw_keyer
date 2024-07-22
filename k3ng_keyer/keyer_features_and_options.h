9a233afc99cc9f4732cdbabe45b5bae69c67baf4
Lance Conry <lance@rhinosw.com>
2024-07-22 12:12:39 +1000

    Added OLED_SSD1306 feature with dimention settings

--- k3ng_keyer/keyer_features_and_options.h
+++ k3ng_keyer/keyer_features_and_options.h
@@ -30,6 +30,7 @@
 // #define FEATURE_LCD1602_N07DH      // http://linksprite.com/wiki/index.php5?title=16_X_2_LCD_Keypad_Shield_for_Arduino
 // #define FEATURE_LCD_SAINSMART_I2C
 // #define FEATURE_LCD_I2C_FDEBRABANDER //https://github.com/fdebrabander/Arduino-LiquidCrystal-I2C-library
+// #define FEATURE_OLED_SSD1306
 // #define FEATURE_CW_DECODER              // https://github.com/k3ng/k3ng_cw_keyer/wiki/385-Feature:-CW-Decoder 
 // #define FEATURE_SLEEP                   // go to sleep after x minutes to conserve battery power (not compatible with Arduino DUE, may have mixed results with Mega and Mega ADK)
 // #define FEATURE_LCD_BACKLIGHT_AUTO_DIM  // turn off LCD backlight and/or dim Power Indicator LED after x minutes (LED requires a PWM pin)
